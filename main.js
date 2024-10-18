34b5c799d134e70b5a980ba1bafbb492c38649db
Html <yaboelwafa289@gmail.com>
2024-10-18 21:26:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+let contnaier = document.querySelector('.contnaier');
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    contnaier.style.background = color;
+}
